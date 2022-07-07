ddc6e77aaf00e6e3c8945c4893d72514cab85073
Farhan Rahman <farhanrahman19@yahoo.co.uk>
2022-07-07 09:21:38 +0100

    Fix node visit order error

--- src/Algorithms/algoUtil.js
+++ src/Algorithms/algoUtil.js
@@ -2,12 +2,14 @@
   // Backtracks from the finishNode to find the shortest path.
   // Only works when called *after* the dijkstra method above.
   function makeShortestPath(goalNode) {
+    console.log(goalNode);
     const shortestPath = [];
     let currentNode = goalNode;
     while (currentNode !== null) {
       shortestPath.unshift(currentNode);
       currentNode = currentNode.previousNode;
     }
+    console.log(shortestPath);
     return shortestPath;
   }
 
