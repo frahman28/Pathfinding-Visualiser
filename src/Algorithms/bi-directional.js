2c5a04a66ec7ef6efec7ec6afbaa7027eae10259
Farhan Rahman <farhanrahman19@yahoo.co.uk>
2022-07-08 09:40:37 +0100

    Fix distance calculation

---
+++ src/Algorithms/bi-directional.js
@@ -0,0 +1,77 @@
+export function expandBoth(grid, startNode, goalNode) {
+    const visitedNodesStart = [];
+    const visitedNodesGoal = [];
+    const visitedNodes = [[], []];
+    let queueStart = [startNode];
+    let queueGoal = [goalNode];
+    while (queueStart.length !== 0 && queueGoal.length !== 0) {
+        let nearestNodeStart = queueStart.shift();
+        let nearestNodeGoal = queueGoal.shift();
+        console.log(nearestNodeGoal);
+        if (nearestNodeStart.isWall) continue;
+        if (nearestNodeGoal.isWall) continue;
+        nearestNodeStart.isVisited = true;
+        nearestNodeGoal.isVisited = true;
+        let childrenNodesStart = getNearestNodes(nearestNodeStart, grid);
+        let childrenNodesGoal = getNearestNodes(nearestNodeGoal, grid);
+        queueStart = queueStart.concat(childrenNodesStart);
+        queueGoal = queueGoal.concat(childrenNodesGoal);
+        visitedNodes[0].push(nearestNodeStart);
+        visitedNodes[1].push(nearestNodeGoal);
+        if (nearestNodeStart === goalNode) return visitedNodes;
+        if (nearestNodeGoal === startNode) return visitedNodes;
+        queueStart = new Set(queueStart);
+        queueGoal = new Set(queueGoal);
+        queueStart = Array.from(queueStart);
+        queueGoal = Array.from(queueGoal);
+    }
+    return visitedNodes;
+}
+
+export function expandStart(grid, startNode, goalNode) {
+    const visitedNodes = [];
+    let queue = [startNode];
+    while (queue.length !== 0) {
+        let nearestNode = queue.shift();
+        if (nearestNode.isWall) continue;
+        nearestNode.isVisited = true;
+        let childrenNodes = getNearestNodes(nearestNode, grid);
+        queue = queue.concat(childrenNodes);
+        visitedNodes.push(nearestNode);
+        if (nearestNode === goalNode) return visitedNodes;
+        queue = new Set(queue);
+        queue = Array.from(queue);
+    }
+    return visitedNodes;
+}
+
+export function expandGoal(grid, startNode, goalNode) {
+    const visitedNodes = [];
+    let queue = [goalNode];
+    while (queue.length !== 0) {
+        let nearestNode = queue.shift();
+        if (nearestNode.isWall) continue;
+        nearestNode.isVisited = true;
+        let childrenNodes = getNearestNodes(nearestNode, grid);
+        queue = queue.concat(childrenNodes);
+        visitedNodes.push(nearestNode);
+        if (nearestNode === startNode) return visitedNodes;
+        queue = new Set(queue);
+        queue = Array.from(queue);
+    }
+    return visitedNodes;
+}
+
+function getNearestNodes(node, grid) {
+    let neighbours = []
+    const {column, row} = node;
+    if (column > 0) neighbours.push(grid[row][column - 1]);
+    if (column < grid[0].length - 1) neighbours.push(grid[row][column + 1]);
+    if (row > 0) neighbours.push(grid[row - 1][column]);
+    if (row < grid.length - 1) neighbours.push(grid[row + 1][column]);
+    neighbours = neighbours.filter(neighbour => !neighbour.isVisited);
+    for (const neighbour of neighbours) {
+        neighbour.previousNode = node;
+    }
+    return neighbours;
+}
